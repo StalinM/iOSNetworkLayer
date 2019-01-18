29e528f887d627f7ef812b66a63ecd6a9e53b4e3
Capgemini <capgemini@Nija.local>
2019-01-18 14:52:37 +0530

    First commit

---
+++ CustomFramework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomFramework
+//
+//  Created by Capgemini on 17/01/19.
+//  Copyright © 2019 Capgemini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
